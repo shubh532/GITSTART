a88110e8873390ab5590b326ac9f435586e85d93
shubh532 <shubhammamahulkar2000>
2023-02-22 11:25:44 +0530

    store  object into local storage

--- myscript.js
+++ myscript.js
@@ -1,17 +1,23 @@
-let form=document.getElementById("form")
+// let form=document.getElementById("form")
 
-form.addEventListener("submit",addtoLocalStorage);
+// form.addEventListener("submit",addtoLocalStorage);
 
-function addtoLocalStorage(e){
-    e.preventDefault();
-    let name=document.getElementById("name").value
-    localStorage.setItem("Name",name)
-    let email=document.getElementById("email").value
-    localStorage.setItem("Email",email)
-    let mob=document.getElementById("mob").value
-    localStorage.setItem("Contact",mob)
-    let date = document.getElementById("date").value
-    localStorage.setItem("Date",date)
+// function addtoLocalStorage(e){
+//     e.preventDefault();
+//     let name=document.getElementById("name").value
+//     localStorage.setItem("Name",name)
+//     let email=document.getElementById("email").value
+//     localStorage.setItem("Email",email)
+//     let mob=document.getElementById("mob").value
+//     localStorage.setItem("Contact",mob)
+//     let date = document.getElementById("date").value
+//     localStorage.setItem("Date",date)
 
+// }
+
+let user_Details = {
+    Name:"Shubham_Mahulkar",
+    Contact: 125364895,
+    age:22
 }
-
+localStorage.setItem("user",JSON.stringify(user_Details))
