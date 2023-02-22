9c676a44ae8d8bad9064f02ad8ed53dbfab38610
shubh532 <shubhammamahulkar2000>
2023-02-22 16:36:51 +0530

    add more user and show it on screen

--- myscript.js
+++ myscript.js
@@ -1,23 +1,42 @@
 let form=document.getElementById("form")
 
 form.addEventListener("submit",addtoLocalStorage);
-
+form.addEventListener("submit",showData);
 function addtoLocalStorage(e){
-    e.preventDefault();
-    let name=document.getElementById("name").value
-    localStorage.setItem("Name",name)
-    let email=document.getElementById("email").value
-    localStorage.setItem("Email",email)
-    let mob=document.getElementById("mob").value
-    localStorage.setItem("Contact",mob)
-    let date = document.getElementById("date").value
-    localStorage.setItem("Date",date)
+     e.preventDefault();
+    // let name=document.getElementById("name").value
+    // localStorage.setItem("Name",name)
+    // let email=document.getElementById("email").value
+    // localStorage.setItem("Email",email)
+    // let mob=document.getElementById("mob").value
+    // localStorage.setItem("Contact",mob)
+    // let date = document.getElementById("date").value
+    // localStorage.setItem("Date",date)
+    
+    //by creating Object
+
+    let user_Details = {
+        Name:document.getElementById("name").value,
+        Email: document.getElementById("email").value,
+        Contact:document.getElementById("mob").value,
+        date:document.getElementById("date").value
+    }
+    localStorage.setItem(document.getElementById("email").value,JSON.stringify(user_Details))
 
 }
 
-let user_Details = {
-    Name:"Shubham_Mahulkar",
-    Contact: 125364895,
-    age:22
+function showData(e){
+    e.preventDefault()
+    let Name=document.getElementById("name").value
+    let Email=document.getElementById("email").value
+    let Contact=document.getElementById("mob").value
+
+    let details =`${Name} - ${Email} - ${Contact}`
+
+
+    let ol=document.getElementById("ol")
+    let li = document.createElement("li")
+    li.appendChild(document.createTextNode(details))
+    ol.appendChild(li)
+    
 }
-localStorage.setItem("user",JSON.stringify(user_Details))
